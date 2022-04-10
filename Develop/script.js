146a0a874104b8915d592e11d9a5b378d91b494c
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-04-10 17:19:53 -0400

    needs older version of bootstrap library

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,56 @@
 // Current Date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+// Save Button
+    $(".saveBtn").on("click", function () {
+// Get nearby values of the description in JQuery
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+// Save text in local storage
+    localStorage.setItem(time, text);
+    })
+
+function timeTracker() {
+// Moment Library
+    var timeNow = moment().hour();
+
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+// To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
+// Local Storage Items
+$("#0800 .description").val(localStorage.getItem("0800"));
+$("#0900 .description").val(localStorage.getItem("0900"));
+$("#1000 .description").val(localStorage.getItem("1000"));
+$("#1100 .description").val(localStorage.getItem("1100"));
+$("#1200 .description").val(localStorage.getItem("1200"));
+$("#1300 .description").val(localStorage.getItem("1300"));
+$("#1400 .description").val(localStorage.getItem("1400"));
+$("#1500 .description").val(localStorage.getItem("1500"));
+$("#1600 .description").val(localStorage.getItem("1600"));
+$("#1700 .description").val(localStorage.getItem("1700"));
+
+timeTracker();
+})
